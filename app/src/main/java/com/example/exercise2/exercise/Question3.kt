4000b4bb00ecf74c36058d9304977ad5513f8024
ekta chandani <ekta.chandani@tothenew.com>
2021-09-22 18:28:30 +0530

    initial commit1

--- app/src/main/java/com/example/exercise2/exercise/Question3.kt
+++ app/src/main/java/com/example/exercise2/exercise/Question3.kt
@@ -15,27 +15,27 @@ interface BankDetails {
 
 class SBI: BankDetails {
     override val ROI: Double
-        get() = 10.0
+        get() = 20.0
     override val bankName: String
         get() = "SBI"
     override val bankCity: String
-        get() = "New Delhi"
+        get() = "Lucknow"
 }
 
 class BOI: BankDetails {
     override val ROI: Double
-        get() = 15.0
+        get() = 25.0
     override val bankName: String
         get() = "BOI"
     override val bankCity: String
-        get() = "Delhi"
+        get() = "Mumbai"
 }
 
 class ICICI: BankDetails {
     override val ROI: Double
-        get() = 8.0
+        get() = 5.0
     override val bankName: String
         get() = "ICICI"
     override val bankCity: String
-        get() = "Noida"
+        get() = "Gurgaon"
 }

efe1ded0e95e48a18f2ec6c0874d8b808339f534
ekta chandani <ekta.chandani@tothenew.com>
2021-09-23 10:56:37 +0530

    initial commit

--- app/src/main/java/com/example/androidbootcamp/MainActivity.kt
+++ app/src/main/java/com/example/androidbootcamp/MainActivity.kt
@@ -1,2 +1,16 @@
 package com.example.androidbootcamp
 
+fun main() {
+
+    // Q3 Create 3 sub class of bank SBI,BOI,ICICI all 4 should have method called getDetails which provide
+    // there specific details like rateofinterest etc,print details of every bank.
+    println("\nQ3")
+    val ob3 = SBI()
+    ob3.getBankDetails()
+    val ob4 = BOI()
+    ob4.getBankDetails()
+    val ob5 = ICICI()
+    ob5.getBankDetails()
+
+
+}
---
+++ app/src/main/java/com/example/androidbootcamp/exercise/Question3.kt
@@ -0,0 +1,41 @@
+package com.example.androidbootcamp
+
+interface BankDetails {
+    val ROI: Double
+    val bankName: String
+    val bankCity: String
+
+    fun getBankDetails() {
+        println("Bank name: $bankName")
+        println("Bank city: $bankCity")
+        println("Rate of Interest: $ROI\n")
+    }
+
+}
+
+class SBI: BankDetails {
+    override val ROI: Double
+        get() = 20.0
+    override val bankName: String
+        get() = "SBI"
+    override val bankCity: String
+        get() = "Lucknow"
+}
+
+class BOI: BankDetails {
+    override val ROI: Double
+        get() = 25.0
+    override val bankName: String
+        get() = "BOI"
+    override val bankCity: String
+        get() = "Mumbai"
+}
+
+class ICICI: BankDetails {
+    override val ROI: Double
+        get() = 5.0
+    override val bankName: String
+        get() = "ICICI"
+    override val bankCity: String
+        get() = "Gurgaon"
+}

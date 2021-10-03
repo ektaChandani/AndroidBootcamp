8540d091be3b4d7e84df8ca83b542ec2e97beb23
ekta chandani <ekta.chandani@tothenew.com>
2021-10-03 23:21:38 +0530

    initial changes in android 01 oct

---
+++ app/src/main/java/com/example/androidbootcamp/LaunchActivity.kt
@@ -0,0 +1,47 @@
+package com.example.androidbootcamp
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Intent
+import android.os.Bundle
+import android.preference.PreferenceFragment
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.preference.PreferenceFragmentCompat
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+
+        // Get views reference
+        val fragDemoBtn = findViewById<Button>(R.id.fragmentDemoBtn)
+        val dialogFragmentBtn = findViewById<Button>(R.id.dFragmentDemoBtn)
+        val preferenceFragmentBtn = findViewById<Button>(R.id.pFragmentDemobtn)
+
+        // attach on click listeners
+        fragDemoBtn.setOnClickListener {
+            // send user to next activity
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        // attach on click listeners
+        dialogFragmentBtn.setOnClickListener {
+            // Get fragment manager support
+            val fragmentManager = supportFragmentManager
+            // create object of class
+            val dialogFragmentDemoObj = DialogFragmentDemo()
+            // show dialog fragment
+            dialogFragmentDemoObj.show(fragmentManager, "Dialog Fragment")
+        }
+
+        // attach on click listeners
+        preferenceFragmentBtn.setOnClickListener {
+            startActivity(Intent(this, PreferenceActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/example/androidbootcamp/MainActivity.kt
+++ app/src/main/java/com/example/androidbootcamp/MainActivity.kt
@@ -1,2 +1,41 @@
 package com.example.androidbootcamp
 
+import android.app.AlertDialog
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.preference.PreferenceFragmentCompat
+
+// Observe Lifecycle of Fragment with activity
+// Observe Lifecycle of Fragment with activity while using Add, Replace, hide, show, remove
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+// Creating Diaglog Fragment
+class DialogFragmentDemo : DialogFragment() {
+
+    // It return Dialog after building it
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setTitle("Welcome")
+            .setMessage("This is Dialog Fragment")
+            .setPositiveButton("Okay!") { _, _ -> }
+            .create()
+}
+
+// Creating PreferenceFragment and
+// inflate the hierarchy
+class MySettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference, rootKey)
+    }
+}

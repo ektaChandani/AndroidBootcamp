d792926fbccf6ddc92cf0c761efc6f852fb3dbcd
ekta chandani <ekta.chandani@tothenew.com>
2021-10-04 18:26:18 +0530

    initial changes in android 04 oct

--- app/src/main/java/com/example/androidbootcamp/LoginActivity.kt
+++ app/src/main/java/com/example/androidbootcamp/LoginActivity.kt
@@ -1,21 +1,18 @@
 package com.example.androidbootcamp
+
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val button: Button = findViewById<Button>(R.id.button1)
+        setContentView(R.layout.activity_login)
+        val button: Button = findViewById<Button>(R.id.button2)
         button.setOnClickListener{
-            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, ThirdActivity::class.java)
             startActivity(intent)
         }
     }
-
-
 }

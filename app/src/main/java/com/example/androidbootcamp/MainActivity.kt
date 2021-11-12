0563d9d44e6d8ea85d6ae246282d922b438abdcd
ekta chandani <ekta.chandani@tothenew.com>
2021-11-12 10:59:45 +0530

    initial changes

--- app/src/main/java/com/example/androidbootcamp/MainActivity.kt
+++ app/src/main/java/com/example/androidbootcamp/MainActivity.kt
@@ -1,2 +1,18 @@
 package com.example.androidbootcamp
 
+import android.os.Bundle
+import android.widget.CheckBox
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //reference checkbox
+        val checkbox = findViewById<CheckBox>(R.id.checkbox1)
+        checkbox.setOnCheckedChangeListener { compoundButton, b -> checkbox.setText(
+            if(compoundButton.isChecked) "Content marked Favourite" else "Content marked unfavourite"
+        ) }
+    }
+}

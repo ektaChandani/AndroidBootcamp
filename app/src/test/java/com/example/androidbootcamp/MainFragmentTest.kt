3995542e0bfcda8856943d28368ca0172754f2ad
ekta chandani <ekta.chandani@tothenew.com>
2021-11-17 10:21:35 +0530

    committed with testing

--- app/src/test/java/com/example/androidbootcamp/MainFragmentTest.kt
+++ app/src/test/java/com/example/androidbootcamp/MainFragmentTest.kt
@@ -1,4 +1,22 @@
 package com.example.androidbootcamp
 
+import android.content.Context
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.runners.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
 class MainFragmentTest {
+    lateinit var mockeditemclick :MainFragment
+    @Mock
+    lateinit var context: Context
+
+
+
+    @Before
+    fun setup(){
+//        context = Instrumentation
+    }
 }
